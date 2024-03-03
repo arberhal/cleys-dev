e73c01f081f4381040804fd8e503c1f028d1b92f
arberhal <arberhalitix@gmail.com>
2024-03-03 20:25:33 +0100

    reverted changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,12 @@
 import Faq from "../components/faq.svelte";
 import Portfolio from "../components/Portfolio.svelte";
 import Services from "../components/services.svelte";
+import Hero from "../components/Hero.svelte";
 </script>
 
+<Hero/>
 <Services/>
-
-<TechnologySection></TechnologySection>
-
+<TechnologySection/>
 <Faq/>
 <Portfolio/>
 
