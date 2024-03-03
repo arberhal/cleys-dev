b3af73fca878728b43b6d8ed53e6ae6fc0eb1d9b
arberhal <arberhalitix@gmail.com>
2024-03-03 17:40:20 +0100

    Add ueberUns component

---
+++ src/components/ueberUns/Customer.svelte
@@ -0,0 +1,18 @@
+<script>
+    import PrimaryButton from "../primary-button.svelte";
+</script>
+
+<section class=" flex lg:justify-center mt-32 lg:mt-60">
+    <div class="lg:w-[1440px] mx-4 lg:mx-16 lg:max-w-[1440px]">
+     <div class="text-display-md text-[#0079C1] font-bold"><h2>Mit unseren Partnern erreichen wir mehr</h2></div>
+     <div>
+        <img src="" alt="">
+        <div>
+            <p><b>Hostpoint AG</b></p>
+            <p class="mt-2">Webhosting-Provider</p>
+            <PrimaryButton text="Mehr erfahren" />
+        </div>
+     </div>
+    </div>
+  </section>
+  
