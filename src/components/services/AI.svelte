7398106912a79b0dcec045fe04b6ab59961a5318
arberhal <arberhalitix@gmail.com>
2024-03-11 19:59:19 +0100

    services page completed

---
+++ src/components/services/AI.svelte
@@ -0,0 +1,21 @@
+<section class="flex lg:justify-center mt-12 mb-32">
+  <div class="mx-4 flex flex-col lg:flex-row lg:mx-16 lg:w-[1440px]">
+    <div class="lg:w-3/5">
+      <div class="text-display-md text-[#0079C1] font-bold lg:text-[52px]">
+        <h2>Künstliche Intelligenz</h2>
+      </div>
+      <div class="text-body mt-2">
+        <p>
+          Unser KI-Service beginnt mit einem gründlichen Verständnis der
+          Herausforderungen und Bedürfnisse Ihres Unternehmens. Wir bieten
+          massgeschneiderte künstliche Intelligenz-Lösungen, die darauf abzielen,
+          Ihre Geschäftsabläufe zu optimieren und die Entscheidungsfindung zu
+          unterstützen. Nachdem wir Ihre Anforderungen genau analysiert haben,
+          präsentieren wir Ihnen einen individuell anpassbaren KI-Service, den
+          Sie bewerten und absegnen können, bevor er nahtlos in Ihre
+          Geschäftsprozesse integriert wird.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ src/routes/dienstleistungen/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Webentwicklung from "../../components/services/Webentwicklung.svelte";
+    import Ai from "../../components/services/AI.svelte";
+    import Seo from "../../components/services/SEO.svelte";
+    import Branding from "../../components/services/Branding.svelte";
+    import Modern from "../../components/services/Modern.svelte";
+    import Ecommerce from "../../components/services/Ecommerce.svelte";
+</script>
+
+<section class="flex lg:justify-center mt-12 h-[80vh] pt-32">
+  <div class="mx-4 flex flex-col lg:flex-row lg:mx-16 lg:w-[1440px]">
+    <div  class="lg:w-3/5">
+      <div class="text-display-md text-[#0079C1] font-bold lg:text-[52px]">
+        <h2>We transform brands through elevated digital experiences</h2>
+      </div>
+      <div class="text-body mt-2">
+        <p>
+          Our rich design and technology expertise delivers top brands and
+          digital experiences.
+        </p>
+      </div>
+    </div>  
+  </div>
+</section>
+<Webentwicklung />
+<Ecommerce />
+<Seo />
+<Branding />
+<Modern/>
+<Ai/>
+
+
