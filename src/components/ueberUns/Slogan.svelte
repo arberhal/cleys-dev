65aa4999a543c64327adb850b95233ba14d0173f
arberhal <arberhalitix@gmail.com>
2025-12-23 23:30:47 +0100

    neu

--- src/components/ueberUns/Slogan.svelte
+++
@@ -1,7 +0,0 @@
-<section class="justify-center lg:flex hidden bg-[#F9FAFF]">
-    <div class="flex flex-col mx-4 md:mx-10 lg:mx-16 lg:max-w-[1280px] w-full my-24">
-      <h2 class="text-[88px] font-medium">
-        Wir sind ein Studio,<br> das innovative Webseiten, Systeme und Anwendungen <br> für Unernehmen entwickelt <br> und gestaltet.
-      </h2>
-    </div>
-    </section>
